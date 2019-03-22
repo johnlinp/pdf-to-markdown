f66dcba6f430db0fdbdecbf538088531c3afa1dc
John Lin <johnlinp@gmail.com>
2019-03-22 13:41:00 +0800

    Use 4 spaces to replace tabs

--- pdf2md/parser.py
+++ pdf2md/parser.py
@@ -10,54 +10,54 @@ from pile import Pile
 
 
 class Parser(object):
-	def __init__(self, filename):
-		self._document = self._read_file(filename)
-		self._device, self._interpreter = self._prepare_tools()
-		self._pages = {}
+    def __init__(self, filename):
+        self._document = self._read_file(filename)
+        self._device, self._interpreter = self._prepare_tools()
+        self._pages = {}
 
-		self._HTML_DEBUG = True
-
-
-	def extract(self, max_page_num=None):
-		for page in PDFPage.create_pages(self._document):
-			self._interpreter.process_page(page)
-			layout = self._device.get_result()
-
-			if max_page_num != None and layout.pageid > max_page_num:
-				break
-
-			self._pages[layout.pageid] = layout
+        self._HTML_DEBUG = True
 
 
-	def parse(self, page_num=None):
-		piles = []
-		if page_num == None:
-			for page_num, page in self._pages.items():
-				piles += self._parse_page(page)
-		else:
-			page = self._pages[page_num]
-			piles = self._parse_page(page)
-		return piles
+    def extract(self, max_page_num=None):
+        for page in PDFPage.create_pages(self._document):
+            self._interpreter.process_page(page)
+            layout = self._device.get_result()
+
+            if max_page_num != None and layout.pageid > max_page_num:
+                break
+
+            self._pages[layout.pageid] = layout
 
 
-	def _read_file(self, filename):
-		parser = PDFParser(open(filename, 'rb'))
-		document = PDFDocument(parser)
-		return document
+    def parse(self, page_num=None):
+        piles = []
+        if page_num == None:
+            for page_num, page in self._pages.items():
+                piles += self._parse_page(page)
+        else:
+            page = self._pages[page_num]
+            piles = self._parse_page(page)
+        return piles
 
 
-	def _prepare_tools(self):
-		laparams = LAParams()
-		rsrcmgr = PDFResourceManager()
-		device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-		interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-		return device, interpreter
+    def _read_file(self, filename):
+        parser = PDFParser(open(filename, 'rb'))
+        document = PDFDocument(parser)
+        return document
 
 
-	def _parse_page(self, page):
-		pile = Pile()
-		pile.parse_layout(page)
-		piles = pile.split_piles()
-		return piles
+    def _prepare_tools(self):
+        laparams = LAParams()
+        rsrcmgr = PDFResourceManager()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        return device, interpreter
+
+
+    def _parse_page(self, page):
+        pile = Pile()
+        pile.parse_layout(page)
+        piles = pile.split_piles()
+        return piles
 
